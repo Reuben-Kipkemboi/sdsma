24bce30f16d683005423da8b449155cf1bb22b18
JosephatNgugi <josephat.muriu@student.moringaschool.com>
2022-07-08 16:39:48 +0300

    add routing links to navbar, routing to login, signup, and homepages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
@@ -9,8 +9,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatGridListModule} from '@angular/material/grid-list';
 // import { NgCircleProgressModule } from 'ng-circle-progress';
 
-import { AppRoutingModule,routingComponents } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { RegisterComponent } from './authentication/register/register.component';
 import { LoginComponent } from './authentication/login/login.component';
 import { StudentComponent } from './student/student.component';
