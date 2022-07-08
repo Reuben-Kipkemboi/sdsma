fcb0e92ce02dabe5c5acf9c6622b74fe9618c834
Ivy Kabere <kabereivy@gmail.com>
2022-07-08 22:18:50 +0300

    fetch likes api;add total number of likes to admin

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { PostsService } from '../_services/posts.service';
 import { Router } from '@angular/router';
 import { CommentsService } from '../services/comments.service'
+import { LikesService } from '../services/likes.service'
+
 
 @Component({
   selector: 'app-admin',
@@ -11,17 +13,20 @@ import { CommentsService } from '../services/comments.service'
 export class AdminComponent implements OnInit {
   postArr: any = [];
   commentlist:any=[];
+  likeslist:any=[];
 
   ngOnInit() {
     this.getPosts();
     this.getComments();
+    this.getLikes();
 
   }
 
   constructor(
     private router: Router,
     private postsService: PostsService,
-    private commentsService: CommentsService
+    private commentsService: CommentsService,
+    private likesService: LikesService,
   ) { }
 
   getPosts() {
@@ -33,6 +38,12 @@ export class AdminComponent implements OnInit {
   getComments() {
     this.commentsService.getComments().subscribe(data => {
       this.commentlist = data;
+    })
+  }
+
+  getLikes() {
+    this.likesService.getLikes().subscribe(data => {
+      this.likeslist = data;
     })
   }
 
