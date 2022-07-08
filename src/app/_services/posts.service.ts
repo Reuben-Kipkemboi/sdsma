66db54724009a73ff598498021be85976c4158da
Dindihub <sadindi03@gmail.com>
2022-07-08 16:46:20 +0300

    Create posts functionality

--- src/app/_services/posts.service.ts
+++ src/app/_services/posts.service.ts
@@ -4,6 +4,7 @@ import { environment } from '../../environments/environment';
 import { Observable } from 'rxjs';
 
 
+
 @Injectable({
   providedIn: 'root',
 })
@@ -13,8 +14,36 @@ export class PostsService {
   constructor(private http: HttpClient) {}
 
   getPosts(): Observable<any> {
-    return this.http.get(
-      environment.apiUrl + '/staff/post/' 
-    );
+    return this.http.get(environment.apiUrl + '/staff/post/');
+  }
+
+  createPosts(
+    content_name: string,
+    content_image:string,
+    video:string,
+    description:string,
+    category:string,) 
+    {
+    return this.http.post(environment.apiUrl + '/staff/post/', {
+      content_name,
+      content_image,
+      video,
+      description,
+      category,
+    });
+  }
+
+  editPosts(val: any) {
+    return this.http.put(this.apiUrl + '/staff/post/', val);
+  }
+  likePosts(val: any) {
+    return this.http.post(this.apiUrl + '/staff/post/', val);
+  }
+  sharePosts(val: any) {
+    return this.http.post(this.apiUrl + '/staff/post/', val);
+  }
+
+  deletePosts(id:number) {
+    return this.http.delete(this.apiUrl + '/post/'+ id);
   }
 }
--- src/app/staff-content-form/staff-content-form.component.ts
+++ src/app/staff-content-form/staff-content-form.component.ts
@@ -1,15 +1,52 @@
+import { AccountService } from './../_services/account.service';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { CategoryService } from '../_services/category.service';
+import { Router } from '@angular/router';
+import { PostsService } from '../_services/posts.service';
 
 @Component({
   selector: 'app-staff-content-form',
   templateUrl: './staff-content-form.component.html',
-  styleUrls: ['./staff-content-form.component.css']
+  styleUrls: ['./staff-content-form.component.css'],
 })
 export class StaffContentFormComponent implements OnInit {
+  constructor(
+    private categoryService: CategoryService,
+    private account: AccountService,
+    private post: PostsService,
+    private router: Router
+  ) {}
 
-  constructor() { }
+  categories: any = [];
+  errorMessages: any;
+  successMessage: any;
+  error = false;
+  loading = false;
+  userId: any;
 
   ngOnInit(): void {
+    
   }
 
+  createPosts(form: NgForm) {
+    let data = form.form.value;
+    this.loading = true;
+    this.post.createPosts(
+      data.content_name,
+      data.content_image,
+      data.video,
+      data.description,
+      data.category
+    )
+      .subscribe(
+        (response) => {
+          form.reset();
+          this.router.navigate(['/staff-page']);
+        })
+       
+    
+  }
+  
 }
+
