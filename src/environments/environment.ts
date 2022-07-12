17a8095e6174d4528539d7e05c6291b86631a3fe
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-07-12 20:08:07 +0300

    ft:user-authentication - fixes user authentication

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,15 +6,7 @@ export const environment = {
   production: false,
   // apiUrl:'https://moti-vate.herokuapp.com'
   // https://software-dev-motivation.herokuapp.com/
-  apiUrl:'https://software-dev-motivation.herokuapp.com'
+  apiUrl:'http://127.0.0.1:8000'
   
 };
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
