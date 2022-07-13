2efacb762013d38a2ea7b65838f1aa46790453f8
JosephatNgugi <josephat.muriu@student.moringaschool.com>
2022-07-13 14:41:31 +0300

    add change budget of deployment

---
+++ src/app/student/std-post/std-comment/std-comment.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-std-comment',
+  templateUrl: './std-comment.component.html',
+  styleUrls: ['./std-comment.component.css']
+})
+export class StdCommentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
